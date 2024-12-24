0e9f76602bb4fbac974949e2c5e612b21ef78ecb
wiggle <86373058+wigglewogglee@users.noreply.github.com>
2024-12-24 01:00:21 -0600

    good work on projects page

--- ProjectManagementApplication/ProjectDAO.cs
+++ ProjectManagementApplication/ProjectDAO.cs
@@ -11,6 +11,9 @@
 
             projects.Add(new Project("project1", 3));
             projects.Add(new Project("project2", 3));
+            projects.Add(new Project("project3", 3));
+            projects.Add(new Project("project4", 3));
+            projects.Add(new Project("project5", 3));
         }
 
         public static List<Project> getProjects()
