89d0dfb408f3af295705f0cbe77cfc813289ce69
wiggle <86373058+wigglewogglee@users.noreply.github.com>
2024-12-23 19:54:55 -0600

    Add project files.

---
+++ ProjectManagementApplication/Project.cs
@@ -0,0 +1,19 @@
+﻿namespace ProjectManagementApplication
+{
+    public class Project
+    {
+
+        public string name { get; set; } = "Project";
+        public int checkpoints { get; set; } = 0;
+
+        public Project()
+        {
+        }
+
+        public Project(string name, int checkpoints)
+        {
+            this.name = name;
+            this.checkpoints = checkpoints;
+        }
+    }
+}
---
+++ ProjectManagementApplication/ProjectDAO.cs
@@ -0,0 +1,23 @@
+﻿namespace ProjectManagementApplication
+{
+    public class ProjectDAO
+    {
+
+        private static List<Project> projects = new();
+        public static void retrieveFromServer()
+        {
+            //server call
+            projects = new List<Project>();
+
+            projects.Add(new Project("project1", 3));
+        }
+
+        public static List<Project> getProjects()
+        {
+
+            retrieveFromServer();
+
+            return projects;
+        }
+    }
+}
